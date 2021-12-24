56891236590f21599d7cf329b1c7e36758bff099
Xt_One <mansur20071903@gmail.com>
2021-12-24 18:40:09 +0500

    Добавление базы данных и ui

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    pass
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Main()
+    ex.show()
+    sys.exit(app.exec())
